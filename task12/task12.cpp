1bb166535cc2e8be82e074be76698492b094d0e3
konovashka <anton_konovalov_1@mail.ru>
2021-11-12 22:57:37 +0300

    Добавьте файлы проекта.

---
+++ task12/task12.cpp
@@ -0,0 +1,27 @@
+﻿// проверка возрастает ли последовательность
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,x,fl,prev;
+	fl = 1;
+	scanf_s("%d", &n);
+	scanf_s("%d", &prev);
+	for (int i = 0; (i < n-1)&&(fl==1); i++)
+	{
+		scanf_s("%d", &x);
+		if (prev>=x)
+		{
+			fl = 0;
+		}
+	}
+	if (fl==1)
+	{
+		printf("yavlyaetsya");
+	}
+	else
+	{
+		printf("ne yavlyaetsya");
+	}
+}
